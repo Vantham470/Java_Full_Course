dfb7dd559300bb9fb943583eaebdc9b7a6e68eb7
Vantham470 <vanthamtyrano@gmail.com>
2026-08-08 00:52:33 +0700

    Add Lesson 04 - Arithmetic

---
+++ 01_Java_Basics/04_Arithmetic/Demo4.java
@@ -0,0 +1,49 @@
+public class Demo4 {
+    public static void main(String[] args) {
+        // 5. Arithmetic
+
+        // int x = 10;
+        // int y = 5;
+        // int z;
+
+
+        // z = x + y;
+        // z = x -y;
+        // z = x * y;
+        // z = x / 5;
+        // z = x % y;
+
+        // Augmented Assignment Operators
+
+        //int x = 10;
+        //int y = 5;
+
+        // x += y;
+        // x -= y;
+        // x *= x;
+        // x /= y;
+        // x %= y;
+
+        // Increment and Decrement Operators
+
+        // int x = 1;
+
+        // x--; // x - 1 = 0
+        // x++; // x + 1 = 2
+
+        // ORDER OF OPERATION [P-E-M-D-A-S]
+        /* P = Parentheses
+           E = exponent
+           M = Multiplication
+           D = Division
+           A = Addition
+           S = Subtraction  
+        */
+
+        double result = 3 + 4 * (7-5) / 2.0;
+
+
+        System.out.print(result);
+        
+    }
+}
