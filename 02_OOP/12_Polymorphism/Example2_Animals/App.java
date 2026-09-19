b3c2ae9c96df2239733fb18bc368460d4d649a03
Vantham470 <vanthamtyrano@gmail.com>
2026-09-19 23:55:55 +0700

    Add OOP: Polymorphism

---
+++ 02_OOP/12_Polymorphism/Example2_Animals/App.java
@@ -0,0 +1,13 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        Animal animal1 = new Dog();
+        Animal animal2 = new Cat();
+        Animal animal3 = new Cow();
+
+        animal1.sound();
+        animal2.sound();
+        animal3.sound();
+    }
+}
---
+++ 02_OOP/12_Polymorphism/Example2_Animals/Dog.java
@@ -0,0 +1,7 @@
+public class Dog implements Animal {
+
+    @Override
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+}
